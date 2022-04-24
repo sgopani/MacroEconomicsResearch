6fb0b22c82d45734f9fd0293fceea87e0a24d348
Satish <satishkathiriya99@gmail.com>
2022-04-24 09:08:01 -0700

    Added agriculture graph

--- app/src/main/java/com/example/macroeconomicsresearch/AnnotationsActivity.java
+++ app/src/main/java/com/example/macroeconomicsresearch/AnnotationsActivity.java
@@ -33,6 +33,8 @@ public class AnnotationsActivity extends AppCompatActivity {
     List<GDPUSD> inflowList = new ArrayList<>();
     List<GDPUSD> outflowList = new ArrayList<>();
     ArrayList<String> checkedStrings;
+    String startYear="1960";
+    String endYear="2020";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +44,8 @@ public class AnnotationsActivity extends AppCompatActivity {
         Button apply_btn=findViewById(R.id.apply_btn);
         EditText end_year_et=findViewById(R.id.end_year_et);
         EditText start_year_et=findViewById(R.id.start_year_et);
+        start_year_et.setText(startYear);
+        end_year_et.setText(endYear);
         Intent intent = getIntent();
         selectedCountry=intent.getStringExtra("selectedCountry");
         selectedUser=intent.getIntExtra("selectedUser",0);
@@ -64,7 +68,15 @@ public class AnnotationsActivity extends AppCompatActivity {
             }
 
         }else if(selectedUser==0 && selectedTable==1) {// user -> resercher & table -> agriculture
-
+            Log.d("Annotation","checkedStrings: " + checkedStrings);
+            if(checkedStrings.contains("Fertilizers Consumption")){
+                Log.d("Annotation","Fertilizer Consumption ");
+                readCsvFertilizaerConsumption(selectedCountry);
+            }
+            if(checkedStrings.contains("Fertilizer Production")){
+                Log.d("Annotation","Fertilizer Production: ");
+                readCsvFertilizaerProduction(selectedCountry);
+            }
         }else if(selectedUser==0 && selectedTable==2){// user -> resercher & table -> debt
 
         }
@@ -86,14 +98,18 @@ public class AnnotationsActivity extends AppCompatActivity {
             annotations_btn.setBackgroundColor(getResources().getColor(R.color.Brown_color));;
             debt_table_image.setBackgroundColor(getResources().getColor(R.color.Brown_color));
         }
+
         apply_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(start_year_et.getText().toString().isEmpty() && !end_year_et.getText().toString().isEmpty()){
+                    start_year_et.setText(startYear);
+                    start_year_et.setText(endYear);
                     Log.e("StartYearEndYear",start_year_et.getText().toString()+ " "+end_year_et.getText().toString());
                 }
             }
         });
+
         annotations_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -258,4 +274,99 @@ public class AnnotationsActivity extends AppCompatActivity {
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(myArray);
         graph.addSeries(series);
     }
+
+    public void readCsvFertilizaerConsumption(String selectedCountry) {
+        InputStream is = getResources().openRawResource(R.raw.fertilizerconsumption);
+        int countryOrder = 1;
+        if(selectedCountry.equals("India")){
+            countryOrder=2;
+        }else if(selectedCountry.equals("China")){
+            countryOrder=1;
+        }else{
+            countryOrder=3;
+        }
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+        String line="";
+        Log.d("Annotation","Column" + countryOrder);
+        try {
+            while ( (line = reader.readLine()) != null){
+                String[] tokens = line.split(",");
+                GDPUSD gdpusd = new GDPUSD();
+                gdpusd.setYear(tokens[0]);
+                double val = new BigDecimal(tokens[countryOrder]).doubleValue();
+                gdpusd.setCountry(val);
+                outflowList.add(gdpusd);
+                Log.d("Annotation","Reading: " + gdpusd);
+            }
+        } catch (IOException e) {
+            Log.wtf("Annotation","error reading csv file" + line ,e);
+            e.printStackTrace();
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.inflow);
+        graph.setVisibility(View.VISIBLE);
+//        graph.setLayoutParams(new LinearLayout.LayoutParams(1000,500));
+        List<DataPoint> list = new ArrayList<>();
+        Log.d("Annotation","Year" + outflowList.get(0).getYear());
+
+        double d = 1961;
+        for(GDPUSD gdpusd : outflowList){
+            list.add(new DataPoint( d++,gdpusd.getCountry()));
+        }
+        DataPoint[] myArray = new DataPoint[list.size()];
+        list.toArray(myArray);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(myArray);
+        graph.addSeries(series);
+    }
+
+    public void readCsvFertilizaerProduction(String selectedCountry) {
+        InputStream is = getResources().openRawResource(R.raw.fertilizersproduction);
+        int countryOrder = 1;
+        if(selectedCountry.equals("India")){
+            countryOrder=2;
+        }else if(selectedCountry.equals("China")){
+            countryOrder=1;
+        }else{
+            countryOrder=3;
+        }
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(is, Charset.forName("UTF-8"))
+        );
+        String line="";
+        Log.d("Annotation","Column" + countryOrder);
+        try {
+            while ( (line = reader.readLine()) != null){
+                String[] tokens = line.split(",");
+                GDPUSD gdpusd = new GDPUSD();
+                gdpusd.setYear(tokens[0]);
+                double val = new BigDecimal(tokens[countryOrder]).doubleValue();
+                gdpusd.setCountry(val);
+                outflowList.add(gdpusd);
+                Log.d("Annotation","Reading: " + gdpusd);
+            }
+        } catch (IOException e) {
+            Log.wtf("Annotation","error reading csv file" + line ,e);
+            e.printStackTrace();
+        }
+
+        GraphView graph = (GraphView) findViewById(R.id.outflow);
+        graph.setVisibility(View.VISIBLE);
+//        graph.setLayoutParams(new LinearLayout.LayoutParams(1000,500));
+        List<DataPoint> list = new ArrayList<>();
+        Log.d("Annotation","Year" + outflowList.get(0).getYear());
+
+        double d = 1960;
+        for(GDPUSD gdpusd : outflowList){
+            list.add(new DataPoint( d++,gdpusd.getCountry()));
+        }
+        DataPoint[] myArray = new DataPoint[list.size()];
+        list.toArray(myArray);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(myArray);
+        graph.addSeries(series);
+    }
+
+
+
 }
